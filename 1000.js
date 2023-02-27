17f0ab0a388de753246dcb54257072add465c4f0
Brendo Abreu <brendosabreu@gmail.com>
2023-02-27 14:58:33 +0000

    Funcionalidade da quantidade de vendas por vendedor e o output da funcionalidade

--- 1000.js
+++ 1000.js
@@ -4,9 +4,10 @@ const DF = require('data-forge-fs');
 
 const listaPedidos = DF.readFileSync('orders.csv').parseCSV().parseInts('quantidade');
 const listaClientes = DF.readFileSync('clients.csv').parseCSV();
+const listaVendedores = DF.readFileSync('sellers.csv').parseCSV();
 
 const escreveArquivo = (dados) => {
-    dados.asCSV().writeFile('1.2 ordem-decrescente.csv');
+    dados.asCSV().writeFile('2.0 vendas-por-vendedores.csv');
 }
 
 let pedidosClientes = listaPedidos
@@ -14,19 +15,36 @@ let pedidosClientes = listaPedidos
     .select(group => ({
         idCliente: group.first()['cliente'],
         totalPedidos: group.deflate(row => row['quantidade']).sum(),
-}))
-.inflate();
+    }))
+    .inflate();
 
 let qtdePedidosPorCliente = pedidosClientes.join(
     listaClientes,
     (left) => left.idCliente,
     (right) => right.id,
     (left,right) => {
-        return {idCliente: left.idCliente, Nome: right.nome, NumPedidos: left.totalPedidos}
+        return {idCliente: left.idCliente, nomeCliente: right.nome, numPedidos: left.totalPedidos}
     }
 );
 
 let pedidosOrdemDecrescente = qtdePedidosPorCliente.orderByDescending(row => row.NumPedidos);
 let pedidosOrdemCrescente = qtdePedidosPorCliente.orderBy(row => row.NumPedidos);
 
-escreveArquivo(pedidosOrdemDecrescente);
+let pedidosVendedor = listaPedidos
+    .groupBy(row => row['vendedor'])
+    .select(group => ({
+        idVendedor: group.first()['vendedor'],
+        totalVendas: group.deflate(row => row['quantidade']).sum(),
+    }))
+    .inflate();
+
+let qntdeVendasPorVendedor = pedidosVendedor.join(
+    listaVendedores,
+    (left) => left.idVendedor,
+    (right) => right.id,
+    (left,right) => {
+        return {idVendedor: left.idVendedor, nomeVendedor: right.nome, numVendas: left.totalVendas}
+    }
+);
+
+escreveArquivo(qntdeVendasPorVendedor);
