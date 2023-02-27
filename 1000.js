96b7150acd50c0eb2128d6c797f7cb4cd32dd57b
Brendo Abreu <brendosabreu@gmail.com>
2023-02-27 13:44:54 +0000

    Funcionalidade para identificar o nome do cliente

--- 1000.js
+++ 1000.js
@@ -3,6 +3,7 @@ const { DataFrame } = require('data-forge');
 const DF = require('data-forge-fs');
 
 const listaPedidos = DF.readFileSync('orders.csv').parseCSV().parseInts('quantidade');
+const listaClientes = DF.readFileSync('clients.csv').parseCSV();
 
 let pedidosClientes = listaPedidos
     .groupBy(row => row['cliente'])
@@ -12,4 +13,14 @@ let pedidosClientes = listaPedidos
 }))
 .inflate();
 
-console.log(pedidosClientes.head(1000).toString());
+let qtdePedidosPorCliente = pedidosClientes.join(
+    listaClientes,
+    (left) => left.idCliente,
+    (right) => right.id,
+    (left,right) => {
+        return {idCliente: left.idCliente, Nome: right.nome, NumPedidos: left.totalPedidos}
+    }
+);
+
+//console.log(qtdePedidosPorCliente.head(5).toString());
+//console.log(listaClientes.head(5).toString());
